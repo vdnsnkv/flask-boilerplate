5a87c72e75d513fa8fd024ef39299f1ed1548ecd
Valentin Denisenkov <denisenkov.valentin@gmail.com>
2021-11-07 20:00:53 +0300

    add handlers module

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 
+import handlers
 from logger import app_logger
+
+
+def configure_handlers(app: Flask) -> None:
+    app.add_url_rule('/', view_func=handlers.index)
+    return
 
 
 def create_app(conf: dict = None) -> Flask:
@@ -11,8 +17,6 @@ def create_app(conf: dict = None) -> Flask:
 
     app.logger = app_logger
 
-    @app.route('/')
-    def index():
-        return ''
+    configure_handlers(app)
 
     return app
